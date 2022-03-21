04a47ef439e0c7723aa95a75a422d8cda3a34c3d
fumiyatake <fumiyatake@gmail.com>
2022-03-22 00:58:02 +0900

    freeze constant

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ const IS_DEV    = process.env.NODE_ENV === 'develop';
 const ROOT_DIR  = IS_DEV ? __dirname : path.join( process.resourcesPath, 'src' );
 const BIN_DIR   = path.join( ROOT_DIR, 'bin' );
 
-module.exports = {
+module.exports = Object.freeze({
     IS_DEV      : IS_DEV,
     ROOT_DIR    : ROOT_DIR,
     BIN_DIR     : BIN_DIR,
-}
+});
