d1910650e42e1bb5f24c68c3bab277c497262deb
fumiyatake <fumiyatake@gmail.com>
2022-04-11 04:34:03 +0900

    ログ消し忘れ

--- src/preload.js
+++ src/preload.js
@@ -6,7 +6,7 @@ const { IS_DEV } = require( './config.js' );
 contextBridge.exposeInMainWorld( 'myApi', {
     selectDirectory : async () => ipcRenderer.invoke( 'select-directory' ),
     convertAnimateImage : async function(){ return ipcRenderer.invoke( 'convert-animate-image', [...arguments] ); },
-    showResult : async function(){ console.log(arguments);return ipcRenderer.invoke( 'show-result', [...arguments] ); }
+    showResult : async function(){ return ipcRenderer.invoke( 'show-result', [...arguments] ); }
 } );
 
 contextBridge.exposeInMainWorld( 'vars', {
